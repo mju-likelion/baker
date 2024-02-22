6795eaca2b84049cb5d2aea743f89ca2e6956d7b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-22 20:54:14 +0900

    setting: Spring Boot 초기화 - #1

---
+++ src/test/java/org/mjulikelion/baker/BakerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.baker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
