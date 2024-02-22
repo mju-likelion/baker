6b107ddc437fd2ef25646e5b122927ecac36371b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-23 01:18:58 +0900

    feat: 지원서 조회 api 추가 - #10
    - RequestParam으로 파트, 페이지를 받음

--- src/main/java/org/mjulikelion/baker/controller/ApplicationController.java
+++ src/main/java/org/mjulikelion/baker/controller/ApplicationController.java
@@ -1,21 +1,25 @@
 package org.mjulikelion.baker.controller;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
-import org.mjulikelion.baker.model.Application;
-import org.mjulikelion.baker.repository.ApplicationRepository;
+import org.mjulikelion.baker.Service.ApplicationService;
+import org.mjulikelion.baker.dto.response.ResponseDto;
+import org.mjulikelion.baker.dto.response.application.ApplicationPageGetResponseData;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("application")
 public class ApplicationController {
-    private final ApplicationRepository applicationRepository;
+    private final ApplicationService applicationService;
 
-    @GetMapping("/test")
-    public List<Application> test() {
-        return this.applicationRepository.findAll();
+    @GetMapping("/{part}")
+    public ResponseEntity<ResponseDto<ApplicationPageGetResponseData>> getApplications(
+            @RequestParam(value = "part") String part,
+            @RequestParam(value = "pageNum") int pageNum) {
+        return applicationService.getApplications(part, --pageNum);// pageNum은 1부터 시작하니 0부터 시작하는 페이지로 변환
     }
 }
