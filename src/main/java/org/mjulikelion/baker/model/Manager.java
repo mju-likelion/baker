45773b522af6a402a4edd2d6cd15e18963db224e
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-26 06:19:28 +0900

    feat: Spring Security 적용을 위한 관리자 Role 구현 - #12

--- src/main/java/org/mjulikelion/baker/model/Manager.java
+++ src/main/java/org/mjulikelion/baker/model/Manager.java
@@ -1,23 +1,86 @@
 package org.mjulikelion.baker.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "manager")
-public class Manager {
+public class Manager implements UserDetails {
     @Id
     @Column(updatable = false, unique = true, nullable = false)
     private String id;
 
     @Column(nullable = false, length = 100)
     private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private List<Role> roles = new ArrayList<>();
+
+    public Manager(String id, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.password = password;
+        this.roles = authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .map(Role::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(Role::toString)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return id;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
