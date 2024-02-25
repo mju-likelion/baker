7b184aae590a312e0631d672cafcbf04e9c92a2a
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-26 06:32:06 +0900

    feat: jwt Filter 구현 - #12

---
+++ src/main/java/org/mjulikelion/baker/filter/JwtFilter.java
@@ -0,0 +1,47 @@
+package org.mjulikelion.baker.filter;
+
+import static org.mjulikelion.baker.constant.SecurityConstant.ACCESS_TOKEN;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.mjulikelion.baker.util.security.JwtEncoder;
+import org.mjulikelion.baker.util.security.JwtTokenProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = resolveTokenFromCookie(request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveTokenFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (ACCESS_TOKEN.equals(cookie.getName())) {
+                    return JwtEncoder.decodeJwtBearerToken(cookie.getValue());
+                }
+            }
+        }
+        return null;
+    }
+}
+
