be3c1b0bd477e69c6d526abeb2e1ae14129e5d13
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-26 06:27:15 +0900

    feat: 상수 추가 - #12

---
+++ src/main/java/org/mjulikelion/baker/constant/EtcConstant.java
@@ -0,0 +1,8 @@
+package org.mjulikelion.baker.constant;
+
+public class EtcConstant {
+    public static final String COLON = " : ";
+    public static final String COMMA = ",";
+    public static final String BLANK = "";
+    public static final String WHITE_SPACE = " ";
+}
