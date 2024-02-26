b3b1ea07ab694e15c70e724f5b2dba79d737ec53
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-27 06:49:23 +0900

    docs: api 경로 RESTful하게 변경 - #22
    - /application?part={part}&pageNum={pageNum} -> /applications?part={part}&pageNum={pageNum}

--- src/main/java/org/mjulikelion/baker/controller/ApplicationController.java
+++ src/main/java/org/mjulikelion/baker/controller/ApplicationController.java
@@ -6,17 +6,15 @@ import org.mjulikelion.baker.dto.response.application.ApplicationPageGetResponse
 import org.mjulikelion.baker.service.application.ApplicationQueryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("application")
 public class ApplicationController {
     private final ApplicationQueryService applicationQueryService;
 
-    @GetMapping()
+    @GetMapping("/applications")
     public ResponseEntity<ResponseDto<ApplicationPageGetResponseData>> getApplications(
             @RequestParam(value = "part") String part,
             @RequestParam(value = "pageNum") int pageNum) {
