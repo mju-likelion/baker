817c9feb7f4ad1b6becb110cc3944ecc77e3b349
Kim Daehyeon <daehyeon3351@gmail.com>
2024-02-28 21:04:37 +0900

    feat: 자기소개 조회 시 지원서 id로 조회하도록 변경, 리팩터링 - #41
    - 학번, 이름 세팅 추가

--- src/main/java/org/mjulikelion/baker/service/introduce/IntroduceQueryService.java
+++ src/main/java/org/mjulikelion/baker/service/introduce/IntroduceQueryService.java
@@ -1,9 +1,10 @@
 package org.mjulikelion.baker.service.introduce;
 
+import java.util.UUID;
 import org.mjulikelion.baker.dto.response.ResponseDto;
 import org.mjulikelion.baker.dto.response.introduce.IntroduceGetResponseData;
 import org.springframework.http.ResponseEntity;
 
 public interface IntroduceQueryService {
-    ResponseEntity<ResponseDto<IntroduceGetResponseData>> getStudentIntroduce(String studentId);
+    ResponseEntity<ResponseDto<IntroduceGetResponseData>> getStudentIntroduce(UUID applicationId);
 }
